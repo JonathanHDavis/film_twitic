dab0f4031cee5ccee9cce098b64ffa30a04c5495
Jonathan Davis <jonathanherscheldavis@gmail.com>
2016-03-30 14:44:20 -0400

    devise and some nav

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,5 +1,5 @@
 class GenresController < ApplicationController
-
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :find_genre, only: [:show, :edit, :update, :destroy]
 
   def index
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :find_movie, only: [:show, :edit, :update, :destroy]
 
   def index
